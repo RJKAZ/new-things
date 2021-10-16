bd8e854fd8b959b8d691d9ea3db4a5387e71b9a5
reese <rjkaz@comcast.net>
2021-10-16 15:44:35 -0400

    express server graphql

---
+++ graphql/server2.js
@@ -0,0 +1,33 @@
+// importing what is needed
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+const { TouchZoomRotateHandler } = require('mapbox-gl');
+
+// construct a schema
+
+var schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+// the root provides a resolver functon for each api endpoint
+var root = {
+  hello: () => {
+    return 'Hello World!';
+  },
+};
+
+var app = express();
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(4000);
+console.log('Runnign a GraphQL API server at http://localhost:4000/graphql');
